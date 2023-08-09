a5009c140a671a6e55bf392001464069c57006e3
Skyler Lewis <skylerlewispg@gmail.com>
2023-08-09 10:02:14 -0700

    exploratory plotting routines and mass calcs

---
+++ R/calcs.R
@@ -0,0 +1,26 @@
+#' @title Calculate invertebrate mass
+#' @description Calculates invertebrate mass (grams) based on field acreage. Requires input of the fields data frame with area_ac attribute. Returns the data frame with additional columns for calculated values. Can be chained with pipes.
+#' @importFrom tidyverse
+#' @importFrom sf
+calc_inv_mass <- function(df, day) {
+  df |>
+    mutate(area_m2 = area_ac / 4047,
+           daily_prod_g = area_m2 * 0.186,
+           total_prod_g = daily_prod_g * day)
+}
+
+#' @title Calculate invertebrate mass time series
+#' @description A wrapper for calc_inv_mass that runs the calculation for a series of elapsed days from 1 to ndays. Returns a data frame with total invertebrate mass (grams) by field unique_id and day.
+#' @importFrom tidyverse
+#' @importFrom sf
+calc_inv_mass_ts <- function(df, ndays) {
+  as_tibble_col(seq(1, ndays), column_name = "day") |>
+    mutate(result = map(day,
+                        function(x){
+                          calc_inv_mass(
+                            st_drop_geometry(df), x) |>
+                            select(unique_id, total_prod_g)
+                          }
+                        )) |>
+    unnest(cols = c(result))
+}
--- R/plot.R
+++ R/plot.R
@@ -1,5 +1,7 @@
 #' @title Plot fields
-#' @description Plot all fields. Display the watersheds, return points, and waterways on the basemap. Returns are distinguished as draining directly into the fish-bearing stream or via a seocndary waterway.
+#' @description Plot all fields. Displays the watersheds, return points, and waterways as well. Returns are distinguished as draining directly into the fish-bearing stream or via a seocndary waterway.
+#' @importFrom tidyverse
+#' @importFrom sf
 plot_fields <- function() {
   ggplot() +
     geom_sf(data = fishFoodMWD::watersheds, color="white") +
@@ -12,9 +14,12 @@ plot_fields <- function() {
 }
 
 #' @title Plot distances
+#' @description Plot all fields showing their calculated flow distances. Distance table is first joined to the fields table. Displays the watersheds, return points, and waterways on the basemap.
+#' @importFrom tidyverse
+#' @importFrom sf
 plot_distances <- function() {
   df <- left_join(fishFoodMWD::fields, fishFoodMWD::distances)
-  ggplot() +
+  ggplot2::ggplot() +
     geom_sf(data = df, aes(fill=totdist_mi, color=totdist_mi)) +
     geom_sf(data = fishFoodMWD::returns) +
     geom_sf(data = fishFoodMWD::canals) +
@@ -24,6 +29,9 @@ plot_distances <- function() {
 }
 
 #' @title Plot watersheds
+#' @description Plot all watersheds (groups) showing their type of flow: lateral to fish-bearing stream, direct to fish-bearing stream via return point outlet, or indirect to fish-bearing stream via return point outlet and secondary canal. Displays the watersheds, return points, and waterways on the basemap.
+#' @importFrom tidyverse
+#' @importFrom sf
 plot_watersheds <- function() {
   df <- left_join(fishFoodMWD::watersheds, st_drop_geometry(fishFoodMWD::returns)) |>
     mutate(category = case_when(is.na(ds_fbs_dist) ~ "Lateral",
@@ -38,3 +46,27 @@ plot_watersheds <- function() {
     scale_fill_manual(values = c("Direct" = "lightblue", "Indirect" = "pink", "Lateral" = "lightyellow")) +
     theme_minimal()
 }
+
+# plot_leaflet <- function() {}
+#
+# SCRATCH AREA
+#
+#plot_by_return_id <- function(id) {
+#  ggplot() +
+#    geom_sf(data = fishFoodMWD::watersheds |> filter(return_id == id)) +
+#    geom_sf(data = left_join(fishFoodMWD::fields, fishFoodMWD::distances) |> filter(return_id == id)) +
+#    geom_sf(data = fishFoodMWD::returns |> filter(return_id == id)) +
+#    theme_minimal()
+#}
+#plot_by_return_id(9)
+#
+#plot_by_watershed <- function() {
+#  ggplot() +
+#    geom_sf(data = left_join(fishFoodMWD::fields, fishFoodMWD::distances),
+#            aes(fill=totdist_mi, color=totdist_mi)) +
+#    geom_sf(data = fishFoodMWD::watersheds, color="white") +
+#    facet_wrap(vars(group_id)) +
+#    theme_minimal()
+#}
+# select a field, highlight the stream, the return_id, the watershed
+# select a return_id, highlight the stream, and all fields and watersheds that drain into it
