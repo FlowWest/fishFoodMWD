2741e378f7480758768b77ab453f697ab769abf3
Skyler Lewis <skylerlewispg@gmail.com>
2023-08-10 17:06:46 -0700

    add data documentation, explicit package calls in plot functions

--- R/calcs.R
+++ R/calcs.R
@@ -1,28 +1,40 @@
 #' @name calc_inv_mass
 #' @title Calculate invertebrate mass
 #' @description Calculates invertebrate mass (grams) based on field acreage. Requires input of the fields data frame with area_ac attribute. Returns the data frame with additional columns for calculated values. Can be chained with pipes.
-#' @importFrom tidyverse
-#' @importFrom sf
+#' @param df An `sf` SpatialPolygonsDataFrame, such as the provided dataframe `fields`, which includes an attribute area_ac containing the field area in acres. Also works with an ordinary data.frame or tibble.
+#' @param day The day number for which to calculate the invertebrate mass.
+#' @export
+#' @examples
+#' # calculate the invertebrate mass after 14 days for each field
+#' fields |> calc_inv_mass(14)
+#' @md
 calc_inv_mass <- function(df, day) {
   df |>
-    mutate(area_m2 = area_ac / 4047,
-           daily_prod_g = area_m2 * 0.186,
-           total_prod_g = daily_prod_g * day)
+    dplyr::mutate(area_m2 = area_ac / 4047,
+                  daily_prod_g = area_m2 * 0.186,
+                  total_prod_g = daily_prod_g * day)
 }
 
 #' @name calc_inv_mass_ts
 #' @title Calculate invertebrate mass time series
-#' @description A wrapper for calc_inv_mass that runs the calculation for a series of elapsed days from 1 to ndays. Returns a data frame with total invertebrate mass (grams) by field unique_id and day.
-#' @importFrom tidyverse
-#' @importFrom sf
+#' @description A wrapper for `calc_inv_mass` that runs the calculation for a series of elapsed days from 1 to ndays. Returns a data frame with total invertebrate mass (grams) by field `unique_id` and `day`.
+#' @param df An `sf` SpatialPolygonsDataFrame, such as the provided dataframe `fields`, which includes an attribute area_ac containing the field area in acres. Also works with an ordinary data.frame or tibble.
+#' @param ndays The total number of days over which to conduct the mass calculation. A sequence of days from 1 to `ndays` is fed into the `day` parameter of the `calc_inv_mass` function.
+#' @export
+#' @examples
+#' # calculate the invertebrate mass time series
+#' fields |> calc_inv_mass_ts(14)
+#' # calculate the invertebrate mass time series for a particular field
+#' fields |> dplyr::filter(unique_id=="1103539") |> calc_inv_mass_ts(14)
+#' @md
 calc_inv_mass_ts <- function(df, ndays) {
-  as_tibble_col(seq(1, ndays), column_name = "day") |>
-    mutate(result = map(day,
+  tibble::as_tibble_col(seq(1, ndays), column_name = "day") |>
+    dplyr::mutate(result = map(day,
                         function(x){
                           calc_inv_mass(
-                            st_drop_geometry(df), x) |>
-                            select(unique_id, total_prod_g)
+                            sf::st_drop_geometry(df), x) |>
+                            dplyr::select(unique_id, total_prod_g)
                           }
                         )) |>
-    unnest(cols = c(result))
+    tidyr::unnest(cols = c(result))
 }
--- R/plot.R
+++ R/plot.R
@@ -1,48 +1,43 @@
 #' @title Plot fields
 #' @description Plot all fields. Displays the watersheds, return points, and waterways as well. Returns are distinguished as draining directly into the fish-bearing stream or via a secondary waterway.
-#' @importFrom tidyverse
-#' @importFrom sf
 plot_fields <- function() {
-  ggplot() +
-    geom_sf(data = fishFoodMWD::watersheds, color="white") +
-    geom_sf(data = fishFoodMWD::fields, color="orange") +
-    geom_sf(data = fishFoodMWD::returns, aes(color=return_direct)) +
-    geom_sf(data = fishFoodMWD::canals, aes(color="Indirect")) +
-    geom_sf(data = fishFoodMWD::streams, aes(color="Direct")) +
-    scale_color_manual(values = c("Direct" = "blue", "Indirect" = "darkred")) +
-    theme_minimal()
+  ggplot2::ggplot() +
+    ggplot2::geom_sf(data = fishFoodMWD::watersheds, color="white") +
+    ggplot2::geom_sf(data = fishFoodMWD::fields, color="orange") +
+    ggplot2::geom_sf(data = fishFoodMWD::returns, aes(color=return_direct)) +
+    ggplot2::geom_sf(data = fishFoodMWD::canals, aes(color="Indirect")) +
+    ggplot2::geom_sf(data = fishFoodMWD::streams, aes(color="Direct")) +
+    ggplot2::scale_color_manual(values = c("Direct" = "blue", "Indirect" = "darkred")) +
+    ggplot2::theme_minimal()
 }
 
 #' @title Plot distances
 #' @description Plot all fields showing their calculated flow distances. Distance table is first joined to the fields table. Displays the watersheds, return points, and waterways on the basemap.
-#' @importFrom tidyverse
-#' @importFrom sf
 plot_distances <- function() {
-  df <- left_join(fishFoodMWD::fields, fishFoodMWD::distances)
+  df <- dplyr::left_join(fishFoodMWD::fields, fishFoodMWD::distances)
   ggplot2::ggplot() +
-    geom_sf(data = df, aes(fill=totdist_mi, color=totdist_mi)) +
-    geom_sf(data = fishFoodMWD::returns) +
-    geom_sf(data = fishFoodMWD::canals) +
-    geom_sf(data = fishFoodMWD::streams) +
-    scale_fill_viridis_c(aesthetics = c("colour", "fill"), direction=-1) +
-    theme_minimal()
+    ggplot2::geom_sf(data = df, aes(fill=totdist_mi, color=totdist_mi)) +
+    ggplot2::geom_sf(data = fishFoodMWD::returns) +
+    ggplot2::geom_sf(data = fishFoodMWD::canals) +
+    ggplot2::geom_sf(data = fishFoodMWD::streams) +
+    ggplot2::scale_fill_viridis_c(aesthetics = c("colour", "fill"), direction=-1) +
+    ggplot2::theme_minimal()
 }
 
 #' @title Plot watersheds
 #' @description Plot all watersheds (groups) showing their type of flow: lateral to fish-bearing stream, direct to fish-bearing stream via return point outlet, or indirect to fish-bearing stream via return point outlet and secondary canal. Displays the watersheds, return points, and waterways on the basemap.
-#' @importFrom tidyverse
-#' @importFrom sf
 plot_watersheds <- function() {
-  df <- left_join(fishFoodMWD::watersheds, st_drop_geometry(fishFoodMWD::returns)) |>
-    mutate(category = case_when(is.na(ds_fbs_dist) ~ "Lateral",
-                                ds_fbs_dist==0 ~ "Direct",
-                                TRUE ~ "Indirect"))
-  ggplot() +
-    geom_sf(data = df, aes(fill=category)) +
-    geom_sf(data = fishFoodMWD::returns, aes(color=return_direct)) +
-    geom_sf(data = fishFoodMWD::canals, aes(color="Indirect")) +
-    geom_sf(data = fishFoodMWD::streams, aes(color="Direct")) +
-    scale_color_manual(values = c("Direct" = "blue", "Indirect" = "darkred")) +
-    scale_fill_manual(values = c("Direct" = "lightblue", "Indirect" = "pink", "Lateral" = "lightyellow")) +
-    theme_minimal()
+  df <- dplyr::left_join(fishFoodMWD::watersheds,
+                         sf::st_drop_geometry(fishFoodMWD::returns)) |>
+    dplyr::mutate(category = dplyr::case_when(is.na(ds_fbs_dist) ~ "Lateral",
+                                              ds_fbs_dist==0 ~ "Direct",
+                                              .default ~ "Indirect"))
+  ggplot2::ggplot() +
+    ggplot2::geom_sf(data = df, aes(fill=category)) +
+    ggplot2::geom_sf(data = fishFoodMWD::returns, aes(color=return_direct)) +
+    ggplot2::geom_sf(data = fishFoodMWD::canals, aes(color="Indirect")) +
+    ggplot2::geom_sf(data = fishFoodMWD::streams, aes(color="Direct")) +
+    ggplot2::scale_color_manual(values = c("Direct" = "blue", "Indirect" = "darkred")) +
+    ggplot2::scale_fill_manual(values = c("Direct" = "lightblue", "Indirect" = "pink", "Lateral" = "lightyellow")) +
+    ggplot2::theme_minimal()
 }
