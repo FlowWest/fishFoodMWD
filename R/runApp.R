aa8635d48747ea1a0a93c3b6b9238ece4815ba72
Inigo Peng <34848367+InigoP@users.noreply.github.com>
2023-08-14 17:50:27 -0700

    added runApp function to start shiny app

---
+++ R/runApp.R
@@ -0,0 +1,15 @@
+#' @title Launch App
+#' @description `run_app()` launches a shiny application that allows the user to
+#' enter data to the run fishFoodMWD functions.
+#' @details the fishFoodMWD application can be run when the user has a dataset that they want to visualize.
+#' @examples
+#' fishFoodMWD::run_app()
+#' @md
+#' @export
+runApp <- function() {
+  appDir <- system.file("app", package = "fishFoodMWD")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `fishFoodMWD`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
