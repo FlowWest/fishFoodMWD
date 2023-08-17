97c8013e576c787f2215a2950c5fbfd2c3ee82e9
Inigo Peng <34848367+InigoP@users.noreply.github.com>
2023-08-16 23:25:29 -0700

    added select point feature from the map

--- inst/app/global.R
+++ inst/app/global.R
@@ -1,5 +1,11 @@
 library(fishFoodMWD)
 library(shiny)
 library(leaflet)
+library(tidyverse)
+library(sf)
 library(dplyr)
 
+geom <- st_coordinates(ff_fields_gcs$geometry)
+# ff_fields
+fields_watersheds <- ff_fields_gcs |>
+  left_join(st_drop_geometry(ff_watersheds_gcs), by="group_id")
