4a94d8e4ebb97bd41cecf47f07646efbaf285f43
Skyler Lewis <skylerlewispg@gmail.com>
2023-08-21 16:17:17 -0700

    pre-join fields dataset to speed up leaflet functions; fix NA values in watersheds

--- data-raw/import_shp.R
+++ data-raw/import_shp.R
@@ -9,7 +9,8 @@ ff_watersheds <-
          huc10,
          watershed_name = name,
          return_id
-  )
+  ) |>
+  mutate(return_id = replace_na(return_id, 0))
 
 # use this layer to define the project CRS
 project_crs <- st_crs(ff_watersheds)
@@ -75,16 +76,25 @@ ff_wetdry <-
   select(wet_dry = hydro) |>
   st_zm()
 
+# FULLY JOINED FIELDS DATASET TO USE IN LEAFLET
+
+ff_fields_joined <- ff_fields |>
+  left_join(sf::st_drop_geometry(ff_watersheds)) |>
+  left_join(sf::st_drop_geometry(ff_returns)) |>
+  left_join(ff_distances) |>
+  mutate(return_direct = case_when(return_direct %in% c("Direct", "Indirect") ~ return_direct, TRUE ~ "Lateral"))
+
 # export tabular datasets
 usethis::use_data(ff_distances, overwrite = TRUE)
 
-#export spatial datasets
+# export spatial datasets
 usethis::use_data(ff_watersheds, overwrite = TRUE)
 usethis::use_data(ff_returns, overwrite = TRUE)
 usethis::use_data(ff_fields, overwrite = TRUE)
 usethis::use_data(ff_streams, overwrite = TRUE)
 usethis::use_data(ff_canals, overwrite = TRUE)
 usethis::use_data(ff_wetdry, overwrite = TRUE)
+usethis::use_data(ff_fields_joined, overwrite = TRUE)
 
 # reproject spatial datasets to WGS84 GCS to use for Leaflet maps
 leaflet_crs <- "+proj=longlat +datum=WGS84"
@@ -95,6 +105,7 @@ ff_fields_gcs <- ff_fields |> st_transform(leaflet_crs)
 ff_streams_gcs <- ff_streams |> st_transform(leaflet_crs)
 ff_canals_gcs <- ff_canals |> st_transform(leaflet_crs)
 ff_wetdry_gcs <- ff_wetdry |> st_transform(leaflet_crs)
+ff_fields_joined_gcs <- ff_fields_joined |> st_transform(leaflet_crs)
 
 ff_watersheds_gcs |> usethis::use_data(overwrite = TRUE)
 ff_returns_gcs |> usethis::use_data(overwrite = TRUE)
@@ -102,4 +113,5 @@ ff_fields_gcs |> usethis::use_data(overwrite = TRUE)
 ff_streams_gcs |> usethis::use_data(overwrite = TRUE)
 ff_canals_gcs |> usethis::use_data(overwrite = TRUE)
 ff_wetdry_gcs |> usethis::use_data(overwrite = TRUE)
+ff_fields_joined_gcs |> usethis::use_data(overwrite = TRUE)
 
