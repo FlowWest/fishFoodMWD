fc71067f653db60dd7ceec01649a7bd261176eb5
Inigo Peng <34848367+InigoP@users.noreply.github.com>
2023-08-21 12:45:07 -0700

    added distance, stream and canals shapefile

--- inst/app/global.R
+++ inst/app/global.R
@@ -10,14 +10,24 @@ geom <- st_coordinates(ff_fields_gcs$geometry)
 fields_watersheds <- ff_fields_gcs |>
   left_join(st_drop_geometry(ff_watersheds_gcs), by="group_id")
 
+fields_returns <- fields_watersheds |>
+  left_join(st_drop_geometry(ff_returns), by="return_id")
+#
+# fields_returns <- fields_returns |>
+#   mutate(return_direct = ifelse(is.na(fields_returns$return_direct), "NA", fields_returns$return_direct))
 
-unique_values<- unique(fields_watersheds$watershed_name)
+fields_distances <- fields_watersheds |>
+  left_join(ff_distances, by="unique_id")
 
-value_color <- Polychrome::palette36.colors(length(unique_values))
 
-value_color_map <- setNames(value_color, unique_values)
+# unique_values<- unique(fields_watersheds$watershed_name)
+#
+# value_color <- Polychrome::palette36.colors(length(unique_values))
+#
+# value_color_map <- setNames(value_color, unique_values)
+#
+# fields_watersheds <- fields_watersheds |> mutate("colour" = value_color_map[watershed_name])
 
-fields_watersheds <- fields_watersheds |> mutate("colour" = value_color_map[watershed_name])
 
 
 
