97ac99a3a844a84d37160b2bbeea00caf1cd98a9
Emanuel Rodriguez <erodriguez@flowwest.com>
2023-08-22 13:46:06 -0700

    wip

--- inst/app/server.R
+++ inst/app/server.R
@@ -3,10 +3,37 @@ function(input, output, session){
   ret_pal <- colorFactor(palette = c("orange", "turquoise"), domain = fields_returns$return_direct, na.color = "#808080")
   dist_pal <- colorNumeric(palette = "Blues", domain = fields_distances$totdist_mi)
 
-  output$field_map <- renderLeaflet({
-    base <- ff_make_leaflet(sf::st_bbox(ff_fields_gcs))
 
-    base |>
+  # using a rective ID option
+  selected_point <- reactiveVal()
+
+  observeEvent(input$field_map_shape_click, {
+    if (!is.null(input$field_map_shape_click$id)) {
+      selected_point(input$field_map_shape_click$id)
+    }
+  })
+
+  # reset the map
+  observeEvent(input$resetButton, {
+    selected_point(NULL)
+  })
+
+  output$field_map <- renderLeaflet({
+    ff_make_leaflet(sf::st_bbox(ff_fields_gcs)) |>
       ff_layer_fields()
+  })
+
+  observe({
+    proxy <- leaflet::leafletProxy("field_map")
+
+    if (!is.null(input$field_map_shape_click$id)) {
+      cat("map marker was clicked\n")
+      cat("click has id: ", input$field_map_shape_click$id, "\n")
+      proxy |>
+        ff_layer_fields(return = input$field_map_shape_click$id)
+    } else {
+      proxy |>
+        ff_layer_fields()
+    }
   })
 }
