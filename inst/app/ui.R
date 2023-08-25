c363faa79fbe28b0523c143599ea9f769e630ee1
Skyler Lewis <skylerlewispg@gmail.com>
2023-08-25 15:08:42 -0700

    ui font and spacing changes to accomodate additional text

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -8,42 +8,37 @@ shinyUI(
     collapsible = TRUE,
     tabPanel("Map"),
     sidebarPanel(
-      width = 2,
+      width = 3,
       tags$h2("Map Controls"),
-      br(),
-      br(),
-      radioButtons(
-        "calculationButton",
-        "Select rice field attribute to display:",
-        c("Return Type" = "return",
-          "Distance" = "distance",
-          "Wet/Dry" = "wetdry",
-          "Invertebrate Mass Days" = "invmass"
-          )
+      div(id = 'mapControls',
+          radioButtons(
+            "calculationButton",
+            "Select rice field attribute to display:",
+            c("Return Type" = "return",
+              "Distance" = "distance",
+              "Wet/Dry" = "wetdry",
+              "Invertebrate Mass Days" = "invmass"
+            )
+          ),
+          conditionalPanel(id = "invmassControlPanel",
+            condition = "input.calculationButton == 'invmass'",
+            numericInput(
+              "inv_mass",
+              "Input the number of days to calculate the invertebrate mass production:",
+              1,
+              min = 1,
+              max = 100),
+            actionButton('runButton' ,'Update Map')),
       ),
-      conditionalPanel(
-        condition = "input.calculationButton == 'invmass'",
-        numericInput(
-          "inv_mass",
-          "Input the number of days to calculate the invertebrate mass production:",
-          1,
-          min = 1,
-          max = 100),
-      br(),
-      actionButton('runButton' ,'Update Map'),
-      ),
-      br(),
-      br(),
-      actionButton(
-        "resetButton",
-        "Reset Map",
-      ),
-      div(id = 'filter_guidance', p("Click a watershed, return point, or rice field to filter.")),
-      div(id = 'reset_guidance', p("Click the map background to reset all filters."), style = "display: none;"),
-      div(id = 'loading_radio', p("Loading data, please wait..."), style = "display: none;"),
+      actionButton("resetButton", "Reset Map"),
+      div(id = 'messages',
+          div(id = 'filter_guidance', "Click a watershed, return point, or rice field to filter."),
+          div(id = 'reset_guidance', "Click the map background to reset all filters.", style = "display: none;"),
+          div(id = 'loading_radio', "Loading data, please wait...", style = "display: none;"),
+      )
     ),
     mainPanel(
-      width = 10, # main width plus sidebar width should add to 12
+      width = 9, # main width plus sidebar width should add to 12
       shinyjs::useShinyjs(),  # Initialize shinyjs
       shinycssloaders::withSpinner(leafletOutput("field_map"))
     )
